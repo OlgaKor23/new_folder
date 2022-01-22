9b487b0402531d550d9e6af86bf47fe264b01811
OlgaKor23 <work.olga.koryshkina@gmail.com>
2022-01-22 15:53:28 +0200

    +

---
+++ build/precache-manifest.2bd86432521085dc03fcbf5b973cedf3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883bd7635576551588ea6bd3aaf66624",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4e57d6c7f9c47c7b8e",
+    "url": "/static/css/main.4058e92a.chunk.css"
+  },
+  {
+    "revision": "28d885eec2b0b67d376f",
+    "url": "/static/js/2.cb2c919e.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.cb2c919e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4e57d6c7f9c47c7b8e",
+    "url": "/static/js/main.788b9fc2.chunk.js"
+  },
+  {
+    "revision": "3cc8b6d8ac9df6218ccf",
+    "url": "/static/js/runtime-main.b65cf54c.js"
+  },
+  {
+    "revision": "97909ca4309bac937f0ddd40f63fdbba",
+    "url": "/static/media/bg-2.97909ca4.png"
+  },
+  {
+    "revision": "5c2940a1f87a9f798c96d19fb6dd88ea",
+    "url": "/static/media/bg-mobile.5c2940a1.png"
+  },
+  {
+    "revision": "25590534d1999eecf857cdd627144e76",
+    "url": "/static/media/bg.25590534.png"
+  },
+  {
+    "revision": "d0ec3c6f3c78d2deb76ee352a951fca7",
+    "url": "/static/media/bonus.d0ec3c6f.png"
+  },
+  {
+    "revision": "cbf1348827eebdc86260cf4c140f180a",
+    "url": "/static/media/br_icon.cbf13488.svg"
+  },
+  {
+    "revision": "bd52f1836340db9c83c9937ef102ed5d",
+    "url": "/static/media/button.bd52f183.png"
+  },
+  {
+    "revision": "dca4e6af7a3ff3e62c9d1ba4fe5ab553",
+    "url": "/static/media/cn_icon.dca4e6af.svg"
+  },
+  {
+    "revision": "1664666d5db82ed347f36336a0493d38",
+    "url": "/static/media/controlleer-icon.1664666d.svg"
+  },
+  {
+    "revision": "2610f9b80df64e8880f9e1b2129d6836",
+    "url": "/static/media/de_icon.2610f9b8.svg"
+  },
+  {
+    "revision": "f6008a7db9d27cb29643bb593358cb94",
+    "url": "/static/media/en_icon.f6008a7d.svg"
+  },
+  {
+    "revision": "5625d3ff3e84502308e3a59690637420",
+    "url": "/static/media/fr_icon.5625d3ff.svg"
+  },
+  {
+    "revision": "74410da3dbf026ab12e6cd70d1cb22ca",
+    "url": "/static/media/it_icon.74410da3.svg"
+  },
+  {
+    "revision": "4ce7537c094dc185451ec19be75166c2",
+    "url": "/static/media/jp_icon.4ce7537c.svg"
+  },
+  {
+    "revision": "be36f745425f1daca3b65967d2b17445",
+    "url": "/static/media/kr_icon.be36f745.svg"
+  },
+  {
+    "revision": "95e803d8656697528377024759e39753",
+    "url": "/static/media/line-01.95e803d8.svg"
+  },
+  {
+    "revision": "a501a620f83ba84f29483bc7114f09be",
+    "url": "/static/media/pl_icon.a501a620.svg"
+  },
+  {
+    "revision": "13c7f45face5203c279bedede816d948",
+    "url": "/static/media/player-icon.13c7f45f.svg"
+  },
+  {
+    "revision": "bccdcc28f4b088c53dd248decca45683",
+    "url": "/static/media/radiation.bccdcc28.svg"
+  },
+  {
+    "revision": "80f60307ef00028a987e4a751a2fe020",
+    "url": "/static/media/ru_icon.80f60307.svg"
+  },
+  {
+    "revision": "9afec321b430d82e7c748e3ce28e2854",
+    "url": "/static/media/sp_icon.9afec321.svg"
+  },
+  {
+    "revision": "f5cac9d47ac04a3d547fe4b70cca2bfd",
+    "url": "/static/media/stalker.f5cac9d4.png"
+  },
+  {
+    "revision": "2cff26ea1671f05a2dcae42fdd8f7c54",
+    "url": "/static/media/triangle.2cff26ea.svg"
+  },
+  {
+    "revision": "faf3e6728bcf064496edc94415f2fca0",
+    "url": "/static/media/ua_icon.faf3e672.svg"
+  },
+  {
+    "revision": "0f393d74a63c95ff847ecda608f06d73",
+    "url": "/static/media/users-icon.0f393d74.svg"
+  }
+]);
---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DocumentList } from "./DocumentList/DocumentList";
+
+const App = () => (
+  <>
+    <DocumentList
+        title={'Rundofase'}
+        description={'Last Edited: 08/08/2020'}
+    />
+  </>
+);
+
+export default App;
