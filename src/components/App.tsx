c52e1fbdbbae86b108ac353b361d8e309df15e22
OlgaKor23 <work.olga.koryshkina@gmail.com>
2022-01-23 16:17:43 +0200

    ++

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,7 @@ import { DocumentList } from "./DocumentList/DocumentList";
 
 const App = () => (
   <>
-    <DocumentList
-        title={'Rundofase'}
-        description={'Last Edited: 08/08/2020'}
-    />
+    <DocumentList/>
   </>
 );
 
